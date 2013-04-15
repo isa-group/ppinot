8eb58b5b4d427dc8bb8f48421f34573ee6249814
Manuel Resinas <resinas@us.es>
2013-04-15 13:43:45 +0200

    Linguistic pattern improved

--- ppinot-templates/src/main/webapp/js/jquery.linguisticpattern.js
+++ ppinot-templates/src/main/webapp/js/jquery.linguisticpattern.js
@@ -3,11 +3,14 @@
 $.fn.linguisticPattern = function(prefix, options, load, onChange) {
     return this.each(function() {
         var dom = $(this);
-        var data = dom.data("value");       
+        var data = dom.data("value");
+        if (typeof prefix == "undefined")
+        	prefix = "";
         if (typeof load == "undefined" && typeof data != "undefined")
         	load = data;
         
-        new LinguisticPattern(prefix, options, dom, onChange).recursiveDisplaySelect(load);
+
+        new LinguisticPattern(prefix, convertOptions(options), dom, onChange).recursiveDisplaySelect(load);
 	});
 	//if (loaded) {
 		
@@ -17,19 +20,79 @@ $.fn.linguisticPattern = function(prefix, options, load, onChange) {
 	//});
 };
 
+function convertOptions(options) {
+	var result = [];
+	for ( var i = 0; i < options.length; i++ ) {
+		if (typeof options[i] == "string")
+			result[i] = {text: options[i]};
+		else if (typeof options[i].appendContent == "undefined")
+			result[i] = convertOption(options[i]);
+		else
+			result[i] = options[i];
+	}
+	return result;
+};
+
+function convertOption(option) {
+	var splits = option.text.split(/{|(?:}\s*{*)/); //}
+	if (splits.length < 2) {
+		return {text: option.text};
+	}
+
+	var result = {
+		text: splits[0],
+		content: {
+			text: splits[0],
+			patterns: []
+		}
+	};
+
+	for (var i = 1; i < splits.length; i++) {
+		if (typeof option[splits[i]] != "undefined") {
+			var pattern = {
+				name: splits[i],
+				options: option[splits[i]].options
+			};
+
+			if (typeof option[splits[i]].prefix != "undefined") {
+				result.text += option[splits[i]].prefix + " ";
+				pattern.prefix = option[splits[i]].prefix;
+			}
+
+			result.text += "{" + splits[i] + "}";
+			result.content.patterns.push(pattern);			
+		}
+	}
+
+	return result;
+};
+
 /*
-options: [{
-	text: "The duration between time instants when <Event> and when <Event>",
-	content: function(option) {
-		return option.text;
+Internal format:
+
+var measure = [
+	{
+		text: "The duration between the time instants when {event} and when {event}",
+		content: {
+			text: "the duration between time instants ",
+			patterns: [
+				{
+					name: "start",
+					prefix: "when",
+					options: eventOptions
+				},
+				{
+					name: "end",
+					prefix: " and when",
+					options: eventOptions
+				}
+			] 
+		}
+	},
+	{
+		text: "The number of times Event"
 	}
-	...
-	content: function(option, container) {
-		str = "The duration between time instants when <div"
-		recursiveDisplaySelect()
-	}
-},
-...]
+];
 */
 
 function LinguisticPattern(prefix, options, elem, onChange) {
@@ -37,22 +100,28 @@ function LinguisticPattern(prefix, options, elem, onChange) {
 	this.options = options;
 	this.elem = elem;
 	this.onChange = onChange;
+	this.previousState = "";
 
 	$(elem).data("value", {});
 };
 
 $.extend(LinguisticPattern.prototype, {
 
+	getValue: function() {
+		return $(this.elem).data("value");
+
+	},
+
 	getCurrentValue: function() {		
-		return $(this.elem).data("value").value;
+		return this.getValue().value;
 	},
 
 	getContainedValue: function() {
-		return $(this.elem).data("value").contained;
+		return this.getValue().contained;
 	},
 
 	setCurrentValue: function(value) {
-		var storedValue = $(this.elem).data("value");
+		var storedValue = this.getValue();
 		storedValue.value = value;
 
 		$(this.elem).data("value", storedValue);
@@ -61,7 +130,7 @@ $.extend(LinguisticPattern.prototype, {
 	},
 
 	setContainedValue: function(value) {
-		var storedValue = $(this.elem).data("value");
+		var storedValue = this.getValue();
 		storedValue.contained = value;
 
 		$(this.elem).data("value", storedValue);
@@ -70,12 +139,12 @@ $.extend(LinguisticPattern.prototype, {
 	},
 
 	setContainedValueByName: function(containedName, value) {
-		var storedValue = $(this.elem).data("value");
+		var storedValue = this.getValue();
 		if (typeof storedValue.contained == "undefined") {
 			storedValue.contained = {};
 		}		
 		storedValue.contained[containedName] = value;
-		$(this.elem).data("containedValue", storedValue);
+		$(this.elem).data("value", storedValue);
 
 		this.notifyChange(storedValue);
 	},
@@ -101,6 +170,7 @@ $.extend(LinguisticPattern.prototype, {
 				that.recursiveDisplaySelect();
 			}
 
+			this.previousState = $(elem).html();
 			$(elem).off("click").attr("tabindex", -1).empty();
 			this.appendPrefix(goBackFunction);
 
@@ -143,15 +213,18 @@ $.extend(LinguisticPattern.prototype, {
 		var prefix = this.prefix;
 		var that = this;
 
+		if (this.getCurrentValue() == value) {
+			load = this.getValue();
+		} else {
+			this.setCurrentValue(value);
+		}
+
 		$(elem).empty();
-		this.setCurrentValue(value);
 
 		this.appendPrefix(goBackFunction);
 
 		if (typeof this.options[value].appendContent != "undefined") {
-			var loadInfo = load;
-			if (typeof load != "undefined")
-				loadInfo = load.contained;
+			var loadInfo = (typeof load != "undefined") ? load.contained : undefined;
 			this.options[value].appendContent(elem, goBackFunction, loadInfo, function(value) {
 				that.setContainedValue(value);
 			});					

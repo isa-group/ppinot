5423be20050be169c8eb37dfbe7ff1f4367ed654
Manuel Resinas <resinas@us.es>
2013-05-23 10:38:31 +0200

    Firefox login bug fixed

--- bpmn-editor/src/main/java/es/us/isa/bpms/users/UserResource.java
+++ bpmn-editor/src/main/java/es/us/isa/bpms/users/UserResource.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * User: resinas
@@ -19,6 +21,7 @@ public class UserResource {
     private UserService userService;
 
     @GET
+    @Produces(MediaType.TEXT_PLAIN)
     public String getLoggedUser() {
         try {
             return userService.getLoggedUser();

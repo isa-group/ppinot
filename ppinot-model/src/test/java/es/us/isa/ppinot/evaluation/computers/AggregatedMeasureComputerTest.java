126bac4cfd803411834671455699264653f89b09
Francisco Martinez Espartero <francisco.mar.esp#gmail.com>
2014-02-20 19:49:16 +0100

    método solo con comentarios, próxima implementación

---
+++ ppinot-model/src/test/java/es/us/isa/ppinot/evaluation/computers/AggregatedMeasureComputerTest.java
@@ -0,0 +1,17 @@
+package es.us.isa.ppinot.evaluation.computers;
+
+import org.junit.Test;
+
+public class AggregatedMeasureComputerTest extends MeasureComputerHelper {
+    @Test
+    public void testCompute() throws Exception {
+    	//LogEntryHelper helper = new LogEntryHelper();
+        //AggregatedMeasureComputer(MeasureDefinition definition, ProcessInstanceFilter filter)
+    	//chooseMeasuresToAggregate
+    	//buildMeasureMap
+    	//update con diferentes ejemplos
+    	
+    	//aserts
+    	
+    }
+}

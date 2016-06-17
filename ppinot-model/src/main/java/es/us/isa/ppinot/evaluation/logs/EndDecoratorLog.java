a6125ea56681e3af1fa1fd3df65347396a30465b
Manuel Resinas <resinas@us.es>
2016-06-17 19:00:07 +0200

    Reorganises decorators of logs to add start and end processes to make them more generic

---
+++ ppinot-model/src/main/java/es/us/isa/ppinot/evaluation/logs/EndDecoratorLog.java
@@ -0,0 +1,65 @@
+package es.us.isa.ppinot.evaluation.logs;
+
+import java.util.*;
+
+/**
+ * StartEndDecoratorLog
+ * Copyright (C) 2015 Universidad de Sevilla
+ *
+ * @author resinas
+ */
+public class EndDecoratorLog extends AbstractLogProvider implements LogProvider, LogListener {
+    private LogProvider sourceLog;
+    private Map<String, LogEntry> finished;
+    private EndMatcher endMatcher;
+    private boolean unknownEnd;
+
+    public EndDecoratorLog(LogProvider sourceLog, EndMatcher endMatcher) {
+        this.sourceLog = sourceLog;
+        this.endMatcher = endMatcher;
+        this.unknownEnd = false;
+        this.finished = new HashMap<String, LogEntry>();
+
+        sourceLog.registerListener(this);
+    }
+
+    public EndDecoratorLog withUnknownEnd() {
+        this.unknownEnd = true;
+        return this;
+    }
+
+    @Override
+    public void processLog() {
+        sourceLog.processLog();
+
+        if (unknownEnd) {
+            for (LogEntry entry : finished.values()) {
+                generateEventAndNotify(entry);
+            }
+        }
+    }
+
+    @Override
+    public void update(LogEntry entry) {
+        updateListeners(entry);
+
+        if (unknownEnd) {
+            if (endMatcher.matches(entry)) {
+                finished.put(entry.getInstanceId(), entry);
+            } else {
+                finished.remove(entry.getInstanceId());
+            }
+        } else {
+            if (endMatcher.matches(entry)) {
+                generateEventAndNotify(entry);
+            }
+        }
+
+    }
+
+    private void generateEventAndNotify(LogEntry entry) {
+        LogEntry endEvent = LogEntry.instance(entry.getProcessId(), entry.getInstanceId(), LogEntry.EventType.complete, entry.getTimeStamp());
+        updateListeners(endEvent);
+    }
+
+}

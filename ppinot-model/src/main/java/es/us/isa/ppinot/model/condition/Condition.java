ea0822d307c18329d81090892d0cedb89196101c
Felipe Vieira da Cunha Serafim <feserafim@gmail.com>
2016-06-29 11:33:47 +0200

    Some minor modifications for ppinot REST

--- ppinot-model/src/main/java/es/us/isa/ppinot/model/condition/Condition.java
+++ ppinot-model/src/main/java/es/us/isa/ppinot/model/condition/Condition.java
@@ -1,5 +1,8 @@
 package es.us.isa.ppinot.model.condition;
 
+import org.codehaus.jackson.annotate.JsonSubTypes;
+import org.codehaus.jackson.annotate.JsonTypeInfo;
+
 /**
  * Applied Software Engineering Research Group (ISA Group) University of
  * Sevilla, Spain
@@ -7,6 +10,14 @@ package es.us.isa.ppinot.model.condition;
  * @author Felipe Vieira da Cunha Serafim <fvieiradacunha@us.es>
  * @version 1.0
  */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.WRAPPER_OBJECT, property = "kind")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = DataPropertyCondition.class, name = "DataPropertyCondition"),
+    @JsonSubTypes.Type(value = StateCondition.class, name = "StateCondition"),
+    @JsonSubTypes.Type(value = TimeInstantCondition.class, name = "TimeInstantCondition"),
+    @JsonSubTypes.Type(value = TimeMeasureType.class, name = "TimeMeasureType")
+})
 public interface Condition {
 
     /**

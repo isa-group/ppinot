8c1be802613bcdf01d3819ed9645cf62dab83010
Felipe Vieira da Cunha Serafim <feserafim@gmail.com>
2017-12-14 10:07:21 +0100

    measures url with https

--- ppinot-model/src/main/java/es/us/isa/ppinot/handler/URLMeasuresCollectionHandler.java
+++ ppinot-model/src/main/java/es/us/isa/ppinot/handler/URLMeasuresCollectionHandler.java
@@ -4,23 +4,30 @@ import es.us.isa.ppinot.model.MeasureDefinition;
 import es.us.isa.ppinot.model.MeasuresCollection;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 /**
- * URLMeasuresCollectionHandler
- * Copyright (C) 2015 Universidad de Sevilla
+ * URLMeasuresCollectionHandler Copyright (C) 2015 Universidad de Sevilla
  *
  * @author resinas
  */
 public class URLMeasuresCollectionHandler {
+
     private static final Logger log = Logger.getLogger(URLMeasuresCollectionHandler.class.getName());
 
     private Map<URL, MeasuresCollection> collectionCache;
@@ -59,7 +66,7 @@ public class URLMeasuresCollectionHandler {
         return measuresCollection;
     }
 
-    private URL cleanURL(URL url)  {
+    private URL cleanURL(URL url) {
         try {
             return new URL(url.getProtocol(), url.getHost(), url.getPort(), url.getFile());
         } catch (MalformedURLException e) {
@@ -69,13 +76,22 @@ public class URLMeasuresCollectionHandler {
     }
 
     public MeasuresCollection downloadCollection(URL url) {
+        if (url.getProtocol().equals("https")) {
+            return downloadCollectionHTTPS(url);
+        } else {
+            return downloadCollectionHTTP(url);
+        }
+    }
+
+    public MeasuresCollection downloadCollectionHTTP(URL url) {
         MeasuresCollection measureDefinitions = new MeasuresCollection();
         URLConnection urlConn = null;
         InputStreamReader in = null;
         try {
             urlConn = url.openConnection();
-            if (urlConn != null)
+            if (urlConn != null) {
                 urlConn.setReadTimeout(60 * 1000);
+            }
             if (urlConn != null && urlConn.getInputStream() != null) {
                 in = new InputStreamReader(urlConn.getInputStream(), Charset.defaultCharset());
                 BufferedReader bufferedReader = new BufferedReader(in);
@@ -88,7 +104,46 @@ public class URLMeasuresCollectionHandler {
                 in.close();
             }
         } catch (Exception e) {
-            throw new RuntimeException("Exception while calling URL:" + url, e);
+            throw new RuntimeException("Exception while calling URL:" + url + " Error: " + e.getMessage());
+        }
+
+        return measureDefinitions;
+    }
+
+    public MeasuresCollection downloadCollectionHTTPS(URL url) {
+        MeasuresCollection measureDefinitions = new MeasuresCollection();
+        try {
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }};
+
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+
+            InputStream is = conn.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+
+            JSONMeasuresCollectionHandler handler = new JSONMeasuresCollectionHandler();
+            handler.load(br, parameters);
+            measureDefinitions = handler.getCollection();
+
+            br.close();
+        } catch (Exception e) {
+            throw new RuntimeException("Exception while calling URL:" + url + " Error: " + e.getMessage());
         }
 
         return measureDefinitions;

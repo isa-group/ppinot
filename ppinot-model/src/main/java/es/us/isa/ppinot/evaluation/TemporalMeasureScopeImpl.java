e188a08357ca13a2e29710cfe864d1d95f904ec3
Manuel Resinas <resinas@us.es>
2018-06-20 16:50:22 +0200

    Extends the comparison of measure scopes to make them independent of time zones

--- ppinot-model/src/main/java/es/us/isa/ppinot/evaluation/TemporalMeasureScopeImpl.java
+++ ppinot-model/src/main/java/es/us/isa/ppinot/evaluation/TemporalMeasureScopeImpl.java
@@ -37,6 +37,51 @@ public class TemporalMeasureScopeImpl extends MeasureScopeImpl implements Tempor
     }
 
     @Override
+    public boolean equivalentTo(MeasureScope scope) {
+        if (! getInstances().equals(scope.getInstances())) return false;
+        if (! getScopeInfo().keySet().equals(scope.getScopeInfo())) return false;
+
+        for (Map.Entry<String, Object> entry : getScopeInfo().entrySet()) {
+            if (! existsIdenticalEntry(entry, scope)) return false;
+        }
+
+        return true;
+    }
+
+
+    @Override
+    public boolean isContainedIn(MeasureScope scope) {
+        for (Map.Entry<String, Object> entry : getScopeInfo().entrySet()) {
+            if (! scope.getScopeInfo().containsKey(entry.getKey())) return false;
+            if (! existsIdenticalEntry(entry, scope)) return false;
+        }
+
+        if (!scope.getInstances().containsAll(getInstances())) return false;
+
+        return true;
+    }
+
+    private boolean existsIdenticalEntry(Map.Entry<String, Object> entry, MeasureScope scope) {
+        Object otherValue = scope.getScopeInfo().get(entry.getKey());
+        if (entry.getValue() == null &&  otherValue != null) return false;
+        if (entry.getValue() != null) {
+            if ("start".equals(entry.getKey()) || "end".equals(entry.getKey())) {
+                try {
+                    DateTime other = (DateTime) otherValue;
+                    DateTime mine = (DateTime) entry.getValue();
+                    if (!mine.isEqual(other)) return false;
+                } catch (Exception e) {
+                    return false;
+                }
+            } else {
+                if (!entry.getValue().equals(otherValue)) return false;
+
+            }
+        }
+        return true;
+    }
+
+    @Override
     public DateTime getStart() {
         return start;
     }
